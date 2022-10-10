ff8ac3a60316d0b2e2a28aebfa417bdffaa1e84d
Joseph D Rivera <josephdrivera15@gmail.com>
2022-10-10 12:01:25 -0400

    all fix no ids

--- Services/randomJokeService.js
+++ Services/randomJokeService.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
+require('dotenv').config();
 
 const randomJokeService = async () => {
     console.log('Random Joke Service  Service');
-    return axios.get(`${process.env.jokeURL}`).then(result => result.data)
+    return await axios.get(`${process.env.jokeURL}`);
 };
 
 const randomJokeServiceByid = async (id) => {
     console.log('Random Joke Service By ID Service');
-    return axios.get(`${process.env.jokeURL}${id}`).then(result => result.data)
+    return await axios.get(`${process.env.jokeURL}${id}`);
 };
 
 module.exports = {
--- router/randomJokeRouter.js
+++ router/randomJokeRouter.js
@@ -16,8 +16,8 @@ randomJokeRouter.get("/", (req, res, next) => {
 });
 
 
-randomJokeRouter.get("/:id", (req, res, next) => {
-    const id = req.params.type;
+randomJokeRouter.get("/id:", (req, res, next) => {
+    const id = req.params.id;
     randomJokeServiceByid(id)
         .then(result => res.status(200).json(result.data))
         .catch(err => res.status(500).json({
